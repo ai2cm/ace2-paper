46c1348442a6458205c10f5e950117bd270551c0
Oliver Watt-Meyer <oliverwatt@gmail.com>
2024-07-10 15:03:39 -0700

    Initial inference submission script

---
+++ experiments/evaluator/run.py
@@ -0,0 +1,145 @@
+# requires beaker-py, install with
+# pip install -U beaker-py
+
+import beaker
+import uuid
+from typing import Dict, Any
+import tempfile
+import yaml
+import os
+import fme
+import dacite
+
+IMAGE_NAME = "oliverwm/fme-e48a3777"
+TRAINED_MODEL_DATASET_ID = "01J28DBXV72DQ7XQ3P9X753GN6"
+CHECKPOINT_NAME = "best_inference_ckpt.tar"
+LOCAL_BASE_CONFIG_FILENAME = "base-config.yaml"
+DATASET_CONFIG_FILENAME = "config.yaml"
+DATASET_CONFIG_MOUNTPATH = "/configmount"
+
+
+# experiments defined by overlays which will overwrite the keys of the base config
+DATA_PATH = "/climate-default/2024-06-14-era5-1deg-8layer-1940-2022-netcdfs"
+EXPERIMENT_OVERLAYS = {
+    "era5-10yr": {
+        "n_forward_steps": 14600,
+        "loader": {
+            "start_indices": {"times": ["2001-01-01T00:00:00"]},
+            "dataset": {"data_path": DATA_PATH},
+            "num_data_workers": 8,
+        },
+        "data_writer": {
+            "save_prediction_files": True,
+            "names": ["PRATEsfc", "eastward_wind_0"],
+            "time_coarsen": {"coarsen_factor": 4},
+        },
+    },
+    "era5-80yr": {"n_forward_steps": 116800},
+    "era5-truth-80yr": {
+        "n_forward_steps": 116800,
+        "prediction_loader": {
+            "start_indices": {"times": ["1940-01-01T06:00:00"]},
+            "dataset": {"data_path": DATA_PATH},
+            "num_data_workers": 8,
+        },
+    },
+    "era5-15day-2020": {
+        "n_forward_steps": 60,
+        "forward_steps_in_memory": 1,
+        "loader": {
+            "start_indices": {
+                "first": 116881,
+                "interval": 26,
+                "n_initial_conditions": 50,
+            },
+            "dataset": {"data_path": DATA_PATH},
+            "num_data_workers": 8,
+        },
+    },
+}
+
+
+def write_config_dataset(config: Dict[str, Any]):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        filepath = os.path.join(temp_dir, DATASET_CONFIG_FILENAME)
+        with open(filepath, "w") as f:
+            yaml.safe_dump(config, f)
+        dataset_name = "ace-inference-config-" + str(uuid.uuid4())[:8]
+        dataset = client.dataset.create(dataset_name, filepath)
+    return dataset
+
+
+def get_experiment_spec(
+    name: str,
+    config: Dict[str, Any],
+    image_name=IMAGE_NAME,
+    trained_model_dataset_id=TRAINED_MODEL_DATASET_ID,
+):
+    """Given a dict representing the inference configuration, return a beaker experiment spec."""
+    config_dataset = write_config_dataset(config)
+    env_vars = [
+        beaker.EnvVar(name="WANDB_API_KEY", secret="wandb-api-key"),
+        beaker.EnvVar(name="WANDB_JOB_TYPE", value="inference"),
+        beaker.EnvVar(name="WANDB_NAME", value=name),
+    ]
+    datasets = [
+        beaker.DataMount(
+            source=beaker.DataSource(beaker=config_dataset.id),
+            mount_path=DATASET_CONFIG_MOUNTPATH,
+        ),
+        beaker.DataMount(
+            mount_path="/ckpt.tar",
+            source=beaker.DataSource(beaker=trained_model_dataset_id),
+            sub_path=f"training_checkpoints/{CHECKPOINT_NAME}",
+        ),
+        beaker.DataMount(
+            mount_path="/climate-default",
+            source=beaker.DataSource(weka="climate-default"),
+        ),
+    ]
+    spec = beaker.ExperimentSpec(
+        budget="ai2/climate",
+        description="Do 10-year inference with ACE2 model trained on ERA5.",
+        tasks=[
+            beaker.TaskSpec(
+                name=name,
+                image=beaker.ImageSource(beaker=image_name),
+                command=[
+                    "python",
+                    "-m",
+                    "fme.ace.evaluator",
+                    f"{DATASET_CONFIG_MOUNTPATH}/{DATASET_CONFIG_FILENAME}",
+                ],
+                result=beaker.ResultSpec(path="/output"),
+                resources=beaker.TaskResources(gpu_count=1, shared_memory="50GiB"),
+                context=beaker.TaskContext(priority="normal", preemptible=True),
+                constraints=beaker.Constraints(cluster=["ai2/jupiter-cirrascale-2"]),
+                env_vars=env_vars,
+                datasets=datasets,
+            )
+        ],
+    )
+    return spec
+
+
+if __name__ == "__main__":
+    client = beaker.Beaker.from_env()
+
+    with open(LOCAL_BASE_CONFIG_FILENAME, "r") as f:
+        base_config = yaml.safe_load(f)
+
+    print("Validating that configs have correct types.")
+    for name, overlay in EXPERIMENT_OVERLAYS.items():
+        config = {**base_config, **overlay}
+        print(f"Validating config for experiment {name}.")
+        print(f"Config that is being validated:\n{config}")
+        dacite.from_dict(
+            fme.ace.InferenceEvaluatorConfig, config, config=dacite.Config(strict=True)
+        )
+    print("All configs are valid. Starting experiment submission.")
+    for name, overlay in EXPERIMENT_OVERLAYS.items():
+        config = {**base_config, **overlay}
+        print(f"Creating experiment {name}.")
+        spec = get_experiment_spec(name, config)
+        experiment = client.experiment.create(name, spec)
+        print(f"Experiment created. See https://beaker.org/ex/{experiment.id}")

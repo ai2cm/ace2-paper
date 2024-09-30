ce28cfd4afde9a1094a238f076b3ca2826f3114f
Oliver Watt-Meyer <oliverwatt@gmail.com>
2024-09-30 14:06:33 -0700

    Fix rs3 wandb id

--- notebooks/constants.py
+++ notebooks/constants.py
@@ -6,7 +6,7 @@ ERA5_TRAINING_RUN_WANDB_IDS = {
     "rs0": "2g4hd4u5",
     "rs1": "o952oyir",
     "rs2": "593xn6l8",
-    "rs3": "380pn4s",
+    "rs3": "380pn4sr",
 }
 ERA5_BEST_RUN_WANDB_ID = ERA5_TRAINING_RUN_WANDB_IDS["rs3"]
 

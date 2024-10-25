77e7edf43e839428dd059eb21c19780147a0b21e
Jeremy McGibbon <jeremym@allenai.org>
2024-10-25 14:47:42 -0400

    Updates to 4-degree runs and plots (#35)
    * add period shading for validation/testing
    
    * replace run.py with 1-degree updated version
    
    * don't run ablations and baselines only needed for 1deg
    
    * don't use data writer for monthly files for diagnostic/figure purposes at 1deg
    
    * don't do purely diagnostic runs over the random seed ensemble
    
    * select seed
    
    * updates to 4-degree runs and plots
    
    * fix 10-year reference run
    
    ---------
    
    Co-authored-by: brianhenn <brianhenn@allenai.org>

--- experiments/evaluator-shield-amip-4deg/run.py
+++ experiments/evaluator-shield-amip-4deg/run.py
@@ -10,9 +10,10 @@ import os
 import fme
 import dacite
 
-IMAGE_NAME = "brianhenn/fme-c7a51eff"
-TRAINED_MODEL_DATASET_ID = "01J47JCKPPND0K6GXNP2049NNA"
+IMAGE_NAME = "brianhenn/fme-926fd6e7"
+TRAINED_MODEL_DATASET_ID = "01J4WEM48F3J6Z1CT2ARZ89F4T"
 REFERENCE_DATASET_PATH = "/climate-default/2024-07-24-vertically-resolved-c96-4deg-shield-amip-ensemble-dataset/netCDFs/ic_0001"
+TARGET_DATASET_PATH = "/climate-default/2024-07-24-vertically-resolved-c96-4deg-shield-amip-ensemble-dataset/netCDFs/ic_0002"
 CHECKPOINT_NAME = "best_inference_ckpt.tar"
 LOCAL_BASE_CONFIG_FILENAME = "base-config.yaml"
 DATASET_CONFIG_FILENAME = "config.yaml"
@@ -21,58 +22,151 @@ DATASET_CONFIG_MOUNTPATH = "/configmount"
 
 # experiments defined by overlays which will overwrite the keys of the base config
 EXPERIMENT_OVERLAYS = {
-    "shield-amip-4deg-ace2-inference-10yr-IC0": {
+    "shield-amip-4deg-ace2-inference-10yr-RS4-IC0": {
         "n_forward_steps": 14600,
     },
-    "shield-amip-4deg-ace2-inference-10yr-IC1": {
+    "shield-amip-4deg-ace2-inference-10yr-RS4-IC1": {
         "n_forward_steps": 14600,
         "loader": {
             "start_indices": {"times": ["2001-01-02T00:00:00"]},
         },
     },
-    "shield-amip-4deg-ace2-inference-10yr-IC2": {
+    "shield-amip-4deg-ace2-inference-10yr-RS4-IC2": {
         "n_forward_steps": 14600,
         "loader": {
             "start_indices": {"times": ["2001-01-03T00:00:00"]},
         },
     },
-    "shield-amip-4deg-ace2-inference-82yr-IC0": {
-        "n_forward_steps": 119732,
+    "shield-amip-4deg-ace2-inference-81yr-IC0": {
+        "n_forward_steps": 118341,
         "loader": {
-            "start_indices": {"times": ["1940-01-01T00:00:00"]},
+            "start_indices": {"times": ["1940-01-01T12:00:00"]},
         },
     },
-    "shield-amip-4deg-ace2-inference-82yr-IC1": {
-        "n_forward_steps": 119732,
+    "shield-amip-4deg-ace2-inference-81yr-IC1": {
+        "n_forward_steps": 118341,
         "loader": {
-            "start_indices": {"times": ["1940-01-02T00:00:00"]},
+            "start_indices": {"times": ["1940-01-02T12:00:00"]},
         },
     },
-    "shield-amip-4deg-ace2-inference-82yr-IC2": {
-        "n_forward_steps": 119732,
+    "shield-amip-4deg-ace2-inference-81yr-IC2": {
+        "n_forward_steps": 118341,
         "loader": {
-            "start_indices": {"times": ["1940-01-03T00:00:00"]},
+            "start_indices": {"times": ["1940-01-03T12:00:00"]},
         },
     },
-    "shield-amip-4deg-reference-inference-10yr": {
+    "shield-amip-4deg-reference-inference-10yr-2001": {
         "n_forward_steps": 14600,
         "prediction_loader": {
             "dataset": {"data_path": REFERENCE_DATASET_PATH},
-            "start_indices": {"times": ["2000-01-01T00:00:00"]},
-            "num_data_workers": 8,
+            "start_indices": {"times": ["2001-01-01T00:00:00"]},
         }
     },
-    "shield-amip-4deg-reference-inference-82yr": {
-        "n_forward_steps": 119732,
-        "loader": {"start_indices": {"times": ["1940-01-01T00:00:00"]}},
+    "shield-amip-4deg-reference-inference-81yr": {
+        "n_forward_steps": 118341,
+        "loader": {"start_indices": {"times": ["1940-01-01T12:00:00"]}},
         "prediction_loader": {
             "dataset": {"data_path": REFERENCE_DATASET_PATH},
-            "start_indices": {"times": ["1940-01-01T00:00:00"]},
-            "num_data_workers": 8,
-        }
+            "start_indices": {"times": ["1940-01-01T12:00:00"]},
+        },
     },
 }
 
+# non-best inference checkpoint runs
+RANDOM_SEED_OVERLAYS = {
+    "shield-amip-4deg-ace2-inference-60yr-RS1": (
+        "01J4705XPN5F2WVNBZQZA7RR1E",
+        {
+            "n_forward_steps": 7300,
+            "forward_steps_in_memory": 5,
+            "loader": {
+                "start_indices": {"times": [
+                    "1940-01-01T00:00:00",
+                    "1945-01-01T00:00:00",
+                    "1950-01-01T00:00:00",
+                    "1955-01-01T00:00:00",
+                    "1960-01-01T00:00:00",
+                    "1965-01-01T00:00:00",
+                    "1970-01-01T00:00:00",
+                    "1975-01-01T00:00:00",
+                    "1980-01-01T00:00:00",
+                    "1985-01-01T00:00:00",
+                    "1990-01-01T00:00:00",
+                    "1995-01-01T00:00:00",
+                ]},
+            },
+        },
+    ),
+    "shield-amip-4deg-ace2-inference-60yr-RS2": (
+        "01J47JCKPPND0K6GXNP2049NNA",
+        {
+            "n_forward_steps": 7300,
+            "forward_steps_in_memory": 5,
+            "loader": {
+                "start_indices": {"times": [
+                    "1940-01-01T00:00:00",
+                    "1945-01-01T00:00:00",
+                    "1950-01-01T00:00:00",
+                    "1955-01-01T00:00:00",
+                    "1960-01-01T00:00:00",
+                    "1965-01-01T00:00:00",
+                    "1970-01-01T00:00:00",
+                    "1975-01-01T00:00:00",
+                    "1980-01-01T00:00:00",
+                    "1985-01-01T00:00:00",
+                    "1990-01-01T00:00:00",
+                    "1995-01-01T00:00:00",
+                ]},
+            },
+        },
+    ),
+    "shield-amip-4deg-ace2-inference-60yr-RS3": (
+        "01J47JD9S55Q2HY9WK8YQX414F",
+        {
+            "n_forward_steps": 7300,
+            "forward_steps_in_memory": 5,
+            "loader": {
+                "start_indices": {"times": [
+                    "1940-01-01T00:00:00",
+                    "1945-01-01T00:00:00",
+                    "1950-01-01T00:00:00",
+                    "1955-01-01T00:00:00",
+                    "1960-01-01T00:00:00",
+                    "1965-01-01T00:00:00",
+                    "1970-01-01T00:00:00",
+                    "1975-01-01T00:00:00",
+                    "1980-01-01T00:00:00",
+                    "1985-01-01T00:00:00",
+                    "1990-01-01T00:00:00",
+                    "1995-01-01T00:00:00",
+                ]},
+            },
+        },
+    ),
+    "shield-amip-4deg-ace2-inference-60yr-RS4": (
+        "01J4WEM48F3J6Z1CT2ARZ89F4T",
+        {
+            "n_forward_steps": 7300,
+            "forward_steps_in_memory": 5,
+            "loader": {
+                "start_indices": {"times": [
+                    "1940-01-01T00:00:00",
+                    "1945-01-01T00:00:00",
+                    "1950-01-01T00:00:00",
+                    "1955-01-01T00:00:00",
+                    "1960-01-01T00:00:00",
+                    "1965-01-01T00:00:00",
+                    "1970-01-01T00:00:00",
+                    "1975-01-01T00:00:00",
+                    "1980-01-01T00:00:00",
+                    "1985-01-01T00:00:00",
+                    "1990-01-01T00:00:00",
+                    "1995-01-01T00:00:00",
+                ]},
+            },
+        },
+    ),
+}
 
 def merge_configs(base: Dict[str, Any], new: Dict[str, Any]) -> Dict[str, Any]:
     """Merge nested configurations."""
@@ -140,7 +234,7 @@ def get_experiment_spec(
                 ],
                 result=beaker.ResultSpec(path="/output"),
                 resources=beaker.TaskResources(gpu_count=1, shared_memory="50GiB"),
-                context=beaker.TaskContext(priority="normal", preemptible=True),
+                context=beaker.TaskContext(priority="high", preemptible=True),
                 constraints=beaker.Constraints(cluster=["ai2/jupiter-cirrascale-2"]),
                 env_vars=env_vars,
                 datasets=datasets,
@@ -169,6 +263,32 @@ if __name__ == "__main__":
         config = merge_configs(base_config, overlay)
         print(f"Creating experiment {name}.")
         spec = get_experiment_spec(name, config)
+        try:
+            experiment = client.experiment.create(name, spec, workspace="ai2/ace")
+            print(
+                f"Experiment {name} created. See https://beaker.org/ex/{experiment.id}"
+            )
+        except beaker.exceptions.ExperimentConflict:
+            print(
+                f"Failed to create experiment {name} because it already exists. "
+                "Skipping experiment creation. If you want to submit this experiment, "
+                "delete the existing experiment with the same name, or rename the new "
+                "experiment."
+            )
+
+    for name, (checkpoint, overlay) in RANDOM_SEED_OVERLAYS.items():
+        config = merge_configs(base_config, overlay)
+        print(f"Validating config for experiment {name}.")
+        print(f"Config that is being validated:\n{config}")
+        try:
+            dacite.from_dict(
+                fme.ace.InferenceEvaluatorConfig, config, config=dacite.Config(strict=True)
+            )
+        except:
+            print(f"Error in config for experiment {name}.")
+            break
+        print(f"Creating experiment {name}.")
+        spec = get_experiment_spec(name, config, trained_model_dataset_id=checkpoint)
         try:
             experiment = client.experiment.create(name, spec, workspace="ai2/ace")
             print(

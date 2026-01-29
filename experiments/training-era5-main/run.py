74ef0928460b647d9a94bdd0e1c008ed725c2111
Brian Henn <brianhenn@allenai.org>
2026-01-29 14:44:30 -0800

    use urgent/climate-titan

--- experiments/training-era5-main/run.py
+++ experiments/training-era5-main/run.py
@@ -77,7 +77,7 @@ def get_experiment_spec(name: str, config: Dict[str, Any], image_name=IMAGE_NAME
                 ],
                 result=beaker.ResultSpec(path="/output"),
                 resources=beaker.TaskResources(gpu_count=8, shared_memory="400GiB"),
-                context=beaker.TaskContext(priority="high", preemptible=True),
+                context=beaker.TaskContext(priority="urgent", preemptible=True),
                 constraints=beaker.Constraints(cluster=["ai2/jupiter-cirrascale-2"]),
                 env_vars=env_vars,
                 datasets=datasets,
@@ -105,7 +105,9 @@ if __name__ == "__main__":
         print(f"Creating experiment {name}.")
         spec = get_experiment_spec(name, config)
         try:
-            experiment = client.experiment.create(name, spec, workspace="ai2/ace")
+            experiment = client.experiment.create(
+                name, spec, workspace="ai2/climate-titan"
+            )
             msg = f"Experiment {name} created. https://beaker.org/ex/{experiment.id}"
             print(msg)
         except beaker.exceptions.ExperimentConflict:
